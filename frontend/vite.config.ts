9a0699f75c1fb3e00ca1853b4696b7bcfe8dcdac
Nicolò Monaldini <nicolo.monaldini@gmail.com>
2026-04-21 19:15:37 +0200

    style: format

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), "");
   return {
     plugins: [vue(), vueDevTools()],
     resolve: {
