46141916d2b95a179ab149b9cf358e8cfcdc0c09
Denise Nanni <denise.nanni1@gmail.com>
2026-04-27 21:21:25 +0200

    feat: add user login
    Implement login in the backend folder to authenticate users.
    
    The implementation also returns a JWT token to the frontend.
    
    Co-authored-by: Denise Nanni <denise.nanni1@gmail.com>
    
    refactor(frontend): move login logic to api folder
    
    test: add backend tests

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,12 +3,13 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueDevTools from "vite-plugin-vue-devtools";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
   return {
-    plugins: [vue(), vueDevTools()],
+    plugins: [vue(), vueDevTools(), tailwindcss()],
     resolve: {
       alias: {
         "@": fileURLToPath(new URL("./src", import.meta.url)),
