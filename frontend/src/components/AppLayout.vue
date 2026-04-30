552a698f3c97520e22e36337c2fe453ce65512db
Juri Guglielmi <juri.guglielmi@gmail.com>
2026-04-30 13:23:37 +0200

    feat(frontend): add app layout component

---
+++ frontend/src/components/AppLayout.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import AppHeader from "./AppHeader.vue";
+import AppSidebar from "./AppSidebar.vue";
+</script>
+
+<template>
+  <div class="min-h-screen bg-blue-50 flex flex-col">
+    <AppHeader />
+    <div class="flex flex-1">
+      <AppSidebar />
+      <main class="flex-1 p-6 overflow-auto">
+        <RouterView />
+      </main>
+    </div>
+  </div>
+</template>
