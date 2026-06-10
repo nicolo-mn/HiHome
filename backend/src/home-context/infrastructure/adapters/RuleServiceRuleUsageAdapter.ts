35e25a472d48a79ef0014132590fe542910e3952
Juri Guglielmi <juri.guglielmi@gmail.com>
2026-06-10 11:46:58 +0200

    refactor: moved control of device usage within rules into rules-context

--- backend/src/home-context/infrastructure/adapters/RuleServiceRuleUsageAdapter.ts
+++ backend/src/home-context/infrastructure/adapters/RuleServiceRuleUsageAdapter.ts
@@ -1,7 +1,6 @@
 import { RuleService } from "../../../rule-context/application/services/RuleService";
 import { RuleUsagePort } from "../../application/ports/RuleUsagePort";
 
-// Resolves device-in-rule usage by querying the rule context's RuleService.
 export class RuleServiceRuleUsageAdapter implements RuleUsagePort {
   constructor(private getRuleService: () => RuleService) {}
 
@@ -9,11 +8,9 @@ export class RuleServiceRuleUsageAdapter implements RuleUsagePort {
     homeId: string,
     deviceId: string,
   ): Promise<string[]> {
-    const rules = await this.getRuleService().getRulesForHome(homeId);
-    return rules
-      .filter((rule) =>
-        rule.actions.some((action) => action.getDeviceId() === deviceId),
-      )
-      .map((rule) => rule.name);
+    return await this.getRuleService().getRuleNamesUsingDevice(
+      homeId,
+      deviceId,
+    );
   }
 }
